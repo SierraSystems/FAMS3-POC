a4202344c5913a8e5c11f3014cf37359436e3dd8
rantifamilusi <familusi.babaranti@gmail.com>
2019-10-08 23:06:57 +0100

    create project

---
+++ app/JobManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
