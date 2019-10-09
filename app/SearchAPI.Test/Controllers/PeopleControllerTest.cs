41403cf725cd18227f49fc63d6a073a9efb9c6f3
alex <alexandre.joyeux@nttdata.com>
2019-10-08 21:34:39 -0700

    Adding new People Controller

---
+++ app/SearchAPI.Test/Controllers/PeopleControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SearchAPI.Controllers;
+using SearchAPI.Models;
+
+namespace SearchAPI.Test.Controllers
+{
+    public class PeopleControllerTest
+    {
+
+        private PeopleController sut;
+
+        [SetUp] 
+        public void init()
+        {
+            sut = new PeopleController();
+        }
+
+        [Test]
+        public void with_valid_request_should_accept_request()
+        {
+            var result = (AcceptedResult) sut.Search().Result;
+            Assert.IsInstanceOf<PeopleSearchResponse>(result.Value);
+            Assert.IsNotNull(((PeopleSearchResponse)result.Value).SearchRequestId);
+        }
+
+
+
+    }
+}
---
+++ app/SearchAPI/Models/PeopleSearchResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SearchAPI.Models
+{
+    /// <summary>
+    /// Represents the search request acknowledgment response.
+    /// </summary>
+    public class PeopleSearchResponse
+    {
+        private PeopleSearchResponse(Guid searchRequestId)
+        {
+            this.SearchRequestId = searchRequestId;
+        }
+
+        /// <summary>
+        /// The Unique Identifier of the request.
+        /// </summary>
+        [Required]
+        [Description("The Unique Identifier of the request")]
+        public Guid SearchRequestId { get; }
+
+        /// <summary>
+        /// Creates a new PeopleSearchResponse
+        /// </summary>
+        /// <returns></returns>
+        public static PeopleSearchResponse Create()
+        {
+            return new PeopleSearchResponse(Guid.NewGuid());
+        }
+
+    }
+}
