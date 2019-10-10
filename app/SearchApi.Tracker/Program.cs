a3c56a98fd62c9abb87131c0fac1f8a3d698b1bd
alex <alexandre.joyeux@nttdata.com>
2019-10-10 14:59:16 -0700

    Adding SearchApi Tracker

--- app/SearchApi.Tracker/Program.cs
+++ app/SearchApi.Tracker/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MassTransit;
+using System;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SearchApi.Tracker
 {
@@ -7,6 +10,14 @@ namespace SearchApi.Tracker
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var serviceCollection = new ServiceCollection();
+
+            Startup.ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
         }
+
     }
 }
---
+++ app/SearchApi.Tracker/Startup.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using SearchApi.Core.Options;
+using System;
+
+namespace SearchApi.Tracker
+{
+    public class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        private void ConfigureServiceBus(IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMqSettings = configuration.GetSection(nameof(RabbitMq)).Get<RabbitMq>();
+            var rabbitBaseUri = $"amqp://{rabbitMqSettings.Host}:{rabbitMqSettings.Port}";
+
+            // Register Mass Transit
+            services.AddMassTransit(x =>
+            {
+                // Add RabbitMq Service Bus
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.Host(new Uri(rabbitBaseUri), hostConfigurator =>
+                    {
+                        hostConfigurator.Username(rabbitMqSettings.Username);
+                        hostConfigurator.Password(rabbitMqSettings.Password);
+                    });
+                }));
+            });
+        }
+    }
+}
