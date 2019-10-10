f4092995c8c37ae304a17034f3b1bb57b3a8bac4
Babaranti Familusi <177307@NTTDATA.COM>
2019-10-10 09:39:29 -0600

    wip

---
+++ app/JobManager/Scheduler/Scheduler.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobManager.Scheduler
+{
+    public class Scheduler
+    {
+        public  async Task <IScheduler> CreateScheduler()
+        {
+            NameValueCollection props = new NameValueCollection
+            {  { "quartz.serializer.type", "binary" } };
+
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            return  await factory.GetScheduler();
+        }
+    }
+}
