35205a57fd6b775d1f962f938f0adc5ca50726b0
alex <alexandre.joyeux@nttdata.com>
2019-10-10 08:03:53 -0700

    Add tests

---
+++ app/SearchAPI.Test/Models/InvestigatePersonTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Builders;
+using SearchAPI.Models;
+
+namespace SearchAPI.Test.Models
+{
+    public class InvestigatePersonTest
+    {
+        [Test]
+        public void should_return_an_instence_of_InvestigatePerson()
+        {
+            var sut = InvestigatePerson.Create();
+            Assert.AreNotEqual(default(Guid), sut.OrderId);
+        }
+    }
+}
