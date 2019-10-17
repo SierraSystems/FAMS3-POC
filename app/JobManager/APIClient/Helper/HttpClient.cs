048812a5be3dc4873707b2fdfc155f0935bf4b21
rantifamilusi <familusi.babaranti@gmail.com>
2019-10-17 23:44:51 +0100

    add http client library

---
+++ app/JobManager/APIClient/Helper/HttpClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flurl.Http;
+using System.Threading.Tasks;
+
+namespace JobManager.APIClient.Helper
+{
+   public  class HttpClient
+    {
+        readonly FlurlClient Client;
+        const int HTTP_REQUEST_TIMEOUT = 60;
+        public HttpClient ()
+        {
+            Client = new FlurlClient();
+        }
+
+        public async Task<T> Get<T>(string path, string key = null,object queryParams = null, object headers = null, object cookies = null)
+        {
+
+            try
+            {
+                return await new Flurl.Url(path).SetQueryParams(queryParams ?? new { }).WithClient(Client).WithTimeout(HTTP_REQUEST_TIMEOUT).WithCookies(cookies ?? new { })
+                               .WithHeaders(headers ?? new { })
+                                .WithHeader("Token", key)
+                    .GetAsync().ReceiveJson<T>();
+            }
+            catch (TaskCanceledException ex)
+            {
+                throw ex;
+          
+            }
+        }
+
+
+    }
+}
--- app/SearchAPI/Startup.cs
+++ app/SearchAPI/Startup.cs
@@ -24,7 +24,7 @@ namespace SearchAPI
         {
             services
                 .AddMvc(option => option.EnableEndpointRouting = false)
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             
             services.AddHealthChecks();
 
