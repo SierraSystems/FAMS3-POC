052502f1a35a52c8e31fe1a2bc88ea59a1f56958
alex <alexandre.joyeux@nttdata.com>
2019-10-18 08:56:28 -0700

    Add provider entity framework configuration

---
+++ app/SearchApi.Tracker/Db/ProviderConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SearchApi.Core.Providers;
+
+namespace SearchApi.Tracker.Db
+{
+    /// <summary>
+    /// Provider Entity Framework Configuration
+    /// </summary>
+    public class ProviderConfiguration : IEntityTypeConfiguration<Provider>
+    {
+        public void Configure(EntityTypeBuilder<Provider> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Name).IsUnique();
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
--- app/SearchApi.Tracker/Db/StateMachineContext.cs
+++ app/SearchApi.Tracker/Db/StateMachineContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SearchApi.Core.Providers;
 using SearchApi.Tracker.Tracking;
 
 namespace SearchApi.Tracker.Db
 {
+    /// <summary>
+    /// Represents the state machine context
+    /// </summary>
     public class StateMachineContext : Microsoft.EntityFrameworkCore.DbContext
     {
 
@@ -11,15 +15,20 @@ namespace SearchApi.Tracker.Db
 
         }
 
+        /// <summary>
+        /// Tracks the Investigations
+        /// </summary>
         public DbSet<Investigation> Investigations { get; set; }
 
-        // | firstNAme | lastname
-        // | true      | false
-        
+        /// <summary>
+        /// Tracks the providers
+        /// </summary>
+        public DbSet<Provider> Providers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new InvestigationTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProviderConfiguration());
         }
 
     }
