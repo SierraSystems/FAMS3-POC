fb4855ccaf7b601aacb3c5fa8b221753cb46bf2a
alex <alexandre.joyeux@nttdata.com>
2019-10-18 08:53:37 -0700

    Add new provider class

---
+++ app/SearchApi.Core.Test/Providers/ProviderTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using SearchApi.Core.Providers;
+
+namespace SearchApi.Core.Test.Providers
+{
+    public class ProviderTest
+    {
+
+        [Test]
+        public void it_should_create_a_provider()
+        {
+            var sut = Provider.Create("Test");
+
+            Assert.AreNotEqual(default(Guid), sut.Id);
+            Assert.AreEqual("Test", sut.Name);
+        }
+
+    }
+}
