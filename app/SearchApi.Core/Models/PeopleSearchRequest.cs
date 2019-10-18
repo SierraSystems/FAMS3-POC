fcc5e207b784aad1713ec513141a1ef09ae51892
alex <alexandre.joyeux@nttdata.com>
2019-10-18 14:17:45 -0700

    adding description

--- app/SearchApi.Core/Models/PeopleSearchRequest.cs
+++ app/SearchApi.Core/Models/PeopleSearchRequest.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace SearchApi.Core.Models
 {
+    [Description("A people search request")]
     public class PeopleSearchRequest
     {
-
+   
         [JsonConstructor]
         public PeopleSearchRequest(string firstName, string lastName, string email)
         {
@@ -13,8 +15,11 @@ namespace SearchApi.Core.Models
             this.Email = email;
         }
         
+        [Description("A person first name.")]
         public string FirstName { get; private set; }
+        [Description("A person first name.")]
         public string LastName { get; private set; }
+        [Description("A person email.")]
         public string Email { get; private set; }
 
     }
